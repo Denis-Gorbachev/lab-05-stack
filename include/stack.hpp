5eb9790977478d5da009d967bbcc8318ac5b08e4
Denis Gorbachev <denis.gorbachev2002@yandex.ru>
2022-01-27 03:40:31 +0300

    Final commit

--- include/stack.hpp
+++ include/stack.hpp
@@ -14,32 +14,36 @@ class Stack{
   const T& head() const;
 
  private:
-  T* stack  = nullptr;
+  T* stack  = new T[0];
   int size  = 0;
 };
 
 template <typename T>
 void Stack<T>::push(T&& value){
   size++;
-  T* new_stack = new T(size);
+  T* new_stack = new T[size];
   for (int i = 0; i < size - 1; i++){
     new_stack[i] = stack[i];
   }
   new_stack[size-1] = value;
-  delete stack;
-  stack = new_stack;
+  for (int i = 0; i < size; ++i){
+    stack[i] = new_stack[i];
+  }
+  delete[] new_stack;
 }
 
 template <typename T>
 void Stack<T>::push(const T& value) {
   size++;
-  T* new_stack = new T(size);
+  T* new_stack = new T[size];
   for (int i = 0; i < size - 1; i++){
     new_stack[i] = stack[i];
   }
   new_stack[size-1] = value;
-  delete stack;
-  stack = new_stack;
+  for (int i = 0; i < size; ++i){
+    stack[i] = new_stack[i];
+  }
+  delete[] new_stack;
 }
 
 template <typename T>
@@ -47,12 +51,14 @@ void Stack<T>::pop() {
   if (stack == nullptr)
     throw std::runtime_error("Stack is empty");
   size--;
-  T* new_stack = new T(size);
+  T* new_stack = new T[size];
   for (int i = 0; i < size; i++){
     new_stack[i] = stack[i];
   }
-  delete[] stack;
-  stack = new_stack;
+  for (int i = 0; i < size; ++i){
+    stack[i] = new_stack[i];
+  }
+  delete[] new_stack;
 }
 
 template <typename T>
@@ -72,10 +78,12 @@ void Stack<T>::push_emplace(Args&&... value) {
   for (int i = size; i < size + arg_size; ++i){
     new_stack[i] = args_stack[i - size];
   }
-  delete stack;
   delete[] args_stack;
-  stack = new_stack;
   size += sizeof...(Args);
+  for (int i = 0; i < size; ++i){
+    stack[i] = new_stack[i];
+  }
+  delete[] new_stack;
 }
 
 #endif // INCLUDE_STACK_HPP_
